6a0fa4aa3867167b97add34d0e50943c0b023938
Sadegh Asghari <46051387+sadeqa20@users.noreply.github.com>
2024-03-18 11:02:39 +0330

    Update peformance1.m

--- peformance1.m
+++ peformance1.m
@@ -1,9 +1,9 @@
 function [ Confuse ] = peformance1( confusionMatrix)
+
 [m n]=size(confusionMatrix);
 c=confusionMatrix;
 ss=sum(c,2);
 for i=1:m
     qq(1,i)=c(i,i)/ss(i);
 end
-Confuse=qq;
-    
+Confuse=qq;    
