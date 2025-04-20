1b0d946b7fc4e029402238e0858810ac8bef0481
Sadegh Asghari <46051387+sadeqa20@users.noreply.github.com>
2025-04-20 23:31:20 +0330

    Update Acc.m

--- Acc.m
+++ Acc.m
@@ -4,7 +4,6 @@ for i=1:10
     [acc_rf(i),Recall_rf(i),Precision_rf(i),fmeasure_rf(i),acc_dt(i),Recall_dt(i),Precision_dt(i),fmeasure_dt(i),acc_knn(i),Recall_knn(i),Precision_knn(i),fmeasure_knn(i)]=Classify(data);
 end
 
-
 a_acc_dt=mean(acc_dt);a_acc_knn=mean(acc_knn);a_acc_rf=mean(acc_rf);
 a_Precision_dt=mean(Precision_dt);a_Precision_knn=mean(Precision_knn);a_Precision_rf=mean(Precision_rf);
 a_Recall_dt=mean(Recall_dt);a_Recall_knn=mean(Recall_knn);a_Recall_rf=mean(Recall_rf);
